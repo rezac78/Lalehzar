2507f4c90e1164e496659aa55ebcc830a89d329f
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2024-02-11 18:36:54 +0330

    start Lalehzar

---
+++ src/pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+class MyDocument extends Document {
+        render() {
+                return (
+                        <Html lang="en">
+                                <Head>
+                                        <title>کافه لاله زار</title>
+                                        <meta name="description" content="بهترین طعم ها و خاطره ها" />
+                                        <meta charSet="utf-8" />
+                                        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                                        <link rel="icon" href="/favicon.ico" />
+                                        <link
+                                                href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
+                                                rel="stylesheet"
+                                        />
+                                </Head>
+                                <body>
+                                        <Main />
+                                        <NextScript />
+                                </body>
+                        </Html>
+                );
+        }
+}
+
+export default MyDocument;
