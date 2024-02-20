758ba7f14c3dfb90c3836097d9dae0dbe8d2f72a
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2024-02-20 15:07:53 +0330

    page 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import ImagePart from '@/components/Shared/ImgPart/Image';
-import Links from '@/components/Shared/Link/Link';
+import ImagePart from '@/components/Shared/PartImag/PartImage';
+import PartLink from '@/components/Shared/PartLink/PartLink';
 export default function Custom404() {
         return (
                 <div className="flex flex-col items-center justify-center min-h-screen bg-white dark:bg-black">
                         <div className="text-center">
-                                <ImagePart Src="/notFound.png" width={600} height={400} className="" />
-                                <h1 className="text-6xl font-bold">404</h1>
-                                <p className="text-xl mb-4">Oops! The page you're looking for does not exist.</p>
-                                <Links Href="/" type="font" Title="Go back home" className="text-blue-600 hover:underline" />
+                                <ImagePart src="/notFound.webp" width={600} height={400} className="" />
+                                <h1 className="text-6xl font-bold text-white">404</h1>
+                                <p className="text-xl mb-4 text-white">.اوپس! اون صفحه ای که میخواید وجود نداره</p>
+                                <PartLink Href="/" className="text-blue-600 hover:underline">
+                                        برگشت به صفحه اصلی
+                                </PartLink>
                         </div>
                 </div>
         );
