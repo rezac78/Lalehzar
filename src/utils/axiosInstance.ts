0d7783281df2023d8781025f16fb921354421532
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2024-02-21 18:53:49 +0330

    api

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://lalehzar-api.onrender.com/api",
   timeout: 15000,
   headers: { "Content-Type": "application/json" },
 });
