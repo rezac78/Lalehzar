a477f2c89cc94cbc36661e36815ff039e4dcacde
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2024-02-26 20:40:56 +0330

    fix

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -1,6 +1,14 @@
-import React, { useEffect } from 'react';
+import LoadingPage from '@/components/Shared/Loading/Loading';
+import React, { useEffect, useState } from 'react';
 
 const FlappyBird = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
   useEffect(() => {
     const script = document.createElement('script');
     script.src = '/flappybird.js';
@@ -12,6 +20,9 @@ const FlappyBird = () => {
       document.body.removeChild(script);
     };
   }, []);
+  if (isLoading) {
+    return <LoadingPage />;
+  }
   return (
     <div>
       <canvas className="m-auto mt-2" id="board"></canvas>
